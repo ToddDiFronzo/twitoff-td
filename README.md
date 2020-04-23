9fdc45c27790590afd0645d2f168f89d2f0d7bb0
Todd DiFronzo <fonze@aol.com>
2020-04-23 10:53:20 -0700

    updated

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # twitoff-td
  web application for comparing Twitter users
+
+
+## Installation
+
+TODO: instructions for git clone
+
+## Setup
+
+TODO: instructions for virtual environment
+
+## Usage
+
+'''sh
+
+# Windows:
+export FLASK_APP=hello.py # one-time thing, to set the env var
+flask run                 # use SET instead of export if issues
+'''
--- hello.py
+++ hello.py
@@ -1,6 +1,38 @@
-from flask import Flask
-app = Flask(__name__)
+# from flask import Flask
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+# app = Flask(__name__)
+
+
+# @app.route('/')
+# def hello_world():
+#     print("You visited the Homepage")
+#     return 'Hello, World!'
+
+
+# @app.route("/about")
+# def about():
+#     print("You visited the about page")
+#     return 'About Me (TODO)'
+
+
+# def create_app(config_filename):
+#     app=Flask(__name__)
+#     app.config.from_pyfile(config_filename)
+
+#     from your application.model import db
+#     db.init_app(app)
+
+#     from yourapplication.views.admin import admin
+#     from yourapplication.views.frontend import frontend
+#     app.register_blueprint(admin)
+#     app.register_blueprint(frontend)
+
+#     return app
+
+
+#     from flask import current_app, Blueprint, render_template
+#     admin = Blueprint('admin', __name__, url_prefix='/admin')
+
+#     @admin.route('/')
+#     def index():
+#         return render_template(current_app.config['INDEX_TEMPLATE'])
