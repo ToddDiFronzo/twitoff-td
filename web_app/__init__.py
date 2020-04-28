5f5e168b3fff4223962b6d95d94c81c771d4266a
Todd DiFronzo <fonze@aol.com>
2020-04-28 15:46:08 -0700

    admin routes added

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,6 +6,7 @@ from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.admin_routes import admin_routes 
 
 DATABASE_URI = "sqlite:///C:\\Users\\Todd\\Desktop\\twitoff-td\\twitoff_development.db"
 SECRET_KEY = "super_secret"
@@ -22,6 +23,7 @@ def create_app():
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
+    app.register_blueprint(admin_routes)
 
     return app
 
